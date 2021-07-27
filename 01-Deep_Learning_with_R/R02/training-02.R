707ed5e9b25f67304ccc42d4c7e032e4d9c2afd8
YoshiDesign <30674964+YoshiDesign@users.noreply.github.com>
2021-07-26 21:53:30 -0400

    ignore

---
+++ 01-Deep_Learning_with_R/R02/training-02.R
@@ -0,0 +1,15 @@
+library(keras)
+mnist <- dataset_mnist()
+train_images <- mnist$train$x
+train_labels <- mnist$train$y
+test_images <- mnist$test$x
+test_labels <- mnist$test$y
+
+# Inspect
+length(dim(train_images))
+dim(train_images)
+typeof(train_images)
+
+# Plot
+digit <- train_images[15831,,]
+plot(as.raster(digit, max = 255))
